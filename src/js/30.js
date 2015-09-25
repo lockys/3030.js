5d22c0130d251a62c40691f7e9d8bab191c92d8a
lockys <locky4567@gmail.com>
2015-09-25 10:49:22 +0800

    #should not re-create element for the error page.

--- src/js/30.js
+++ src/js/30.js
@@ -3,27 +3,27 @@
 * Auther: Hao-Wei Jeng
 **/
 ;(function($, window, document, undefined) {
+  var mainPage = $('<div>', {
+        class: 'error-page content-hidden',
+      }),
+    deadImg = $('<img>', {
+      src: 'http://i.imgur.com/l4N7287.png',
+    }),
+    errorDiv = $('<div>', {
+      class: 'error-msg',
+    }),
+    titleStr = '<h2 class="title">Aw, Snap!</h2>',
+    contentEle = $('<div>', {
+      class: 'content',
+      html: 'Something went wrong while displaying this webpage. Closing the Apps and Tabs that you don\'t need may help by making more memory available.<br/><br/>' +
+            'if you\'re seeing this frequently, try this <a class="reload-link" href="">suggestion</a>.' +
+            '<button class="button blue r-btn"> Reload</button>',
+    }),
+    innerErrorDiv = errorDiv.append([deadImg, titleStr, contentEle]),
+    fullPage = mainPage.append(innerErrorDiv);
+
   $.fn.breakThisLink = function(options) {
     options = options || {};
-
-    var mainPage = $('<div>', {
-          class: 'error-page content-hidden',
-        }),
-      deadImg = $('<img>', {
-        src: 'http://i.imgur.com/l4N7287.png',
-      }),
-      errorDiv = $('<div>', {
-        class: 'error-msg',
-      }),
-      titleStr = '<h2 class="title">Aw, Snap!</h2>',
-      contentEle = $('<div>', {
-        class: 'content',
-        html: 'Something went wrong while displaying this webpage. Closing the Apps and Tabs that you don\'t need may help by making more memory available.<br/><br/>' +
-              'if you\'re seeing this frequently, try this <a class="reload-link" href="">suggestion</a>.' +
-              '<button class="button blue r-btn"> Reload</button>',
-      }),
-      innerErrorDiv = errorDiv.append([deadImg, titleStr, contentEle]),
-      fullPage = mainPage.append(innerErrorDiv);
 
     $(this).hover(function() {
       var mainPage = $('.error-page');
